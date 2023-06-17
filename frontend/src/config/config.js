047d331b38593a1f97f61cd6beed708f882c3e08
Kimjimin97 <sja9707@daum.net>
2023-06-17 22:24:17 +0900

    workflow 설정

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -3,4 +3,4 @@
 export const backUrl = "https://greenyday.co.kr/dev";
 // export const backUrl = "http://127.0.0.1:8000";
 // export const frontUrl = "https://greenyday.co.kr";
-// export const frontUrl = "http://localhost:3000";
+export const frontUrl = "http://localhost:3000";
